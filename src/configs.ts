a76fbee876faba8f83a390caa896d059071a6550
Ulises Ramirez <vwalle.r.m@gmail.com>
2020-05-07 14:16:06 -0500

    Funcion derivada

--- src/configs.ts
+++ src/configs.ts
@@ -4,7 +4,13 @@ export const LINE_COLOR = '8E1600';
 
 export const CANVAS_ASPECT_RATIO = 3 / 1;
 export const CANVAS_ID = 'xdfgndhjthfghfghfgh';
-export const evaluateFunction = (x: number) => {
-  return Math.E ** x;
-  // return x ** 2;
+
+export const DECIMALS = 4;
+
+export const evaluateFunction = (x: number): number => {
+  // Function to use in
+  return Number.parseFloat((x ** 3 - x - 1).toFixed(DECIMALS));
+};
+export const derivedFunction = (x: number): number => {
+  return Number.parseFloat((3 * x ** 2 - 1).toFixed(DECIMALS));
 };
